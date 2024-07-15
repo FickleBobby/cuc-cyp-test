58a62eb74e2f906868ac0a77ce8485ceb132966a
FickleBobby <64523325+FickleBobby@users.noreply.github.com>
2024-07-15 16:46:15 +0100

    Update multi-reporter.js

--- multi-reporter.js
+++ multi-reporter.js
@@ -3,6 +3,8 @@ const report = require("multiple-cucumber-html-reporter");
 const fs = require("fs")
 
 function getBrowserDetails() {
+  fs.readdir('./').then(listing => console.log(listing))
+  .catch(err => console.error(err));
     return JSON.parse(fs.readFileSync("./browserDetails.json", "utf-8"));
 }
 const browserDetails = getBrowserDetails();
