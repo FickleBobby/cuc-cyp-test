2a133f1203dc056979bb734be7b3537b08203e85
FickleBobby <64523325+FickleBobby@users.noreply.github.com>
2024-07-15 16:53:52 +0100

    Update multi-reporter.js

--- multi-reporter.js
+++ multi-reporter.js
@@ -1,13 +1,22 @@
 
 const report = require("multiple-cucumber-html-reporter");
 const fs = require("fs")
+var path = require('path');
+
+function fileList(dir) {
+  return fs.readdirSync(dir).reduce(function(list, file) {
+    var name = path.join(dir, file);
+    var isDir = fs.statSync(name).isDirectory();
+    return list.concat(isDir ? fileList(name) : [name]);
+  }, []);
+}
 
 function getBrowserDetails() {
-  fs.readdir('./').then(listing => console.log(listing))
-  .catch(err => console.error(err));
+
     return JSON.parse(fs.readFileSync("./browserDetails.json", "utf-8"));
 }
 const browserDetails = getBrowserDetails();
+console.log(fileList('./'))
 report.generate({
     jsonDir: "./cypress/e2e/reports/json",  // ** Path of .json file **//
     reportPath: "./cypress/e2e/reports/html/reports/multi",
